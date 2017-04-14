e3f9626bdebb0a9ae01d0123229648e91dbdb4d1
Christopher Barry <me@chrisb.xyz>
2017-04-14 11:46:11 -0400

    Hide coverage option

--- flow-ide.py
+++ flow-ide.py
@@ -384,34 +384,34 @@ class FlowListener(sublime_plugin.EventListener):
                     view.set_status('flow_error', error_count_text)
 
         # START OF COVERAGE DISPLAY
-        coverage_result = call_flow_cli(deps.contents, [
-            flow, 'coverage',
-            '--from', 'nuclide',
-            '--json',
-            deps.filename
-        ], view)
+        if settings.get('show_coverage'):
+          coverage_result = call_flow_cli(deps.contents, [
+              flow, 'coverage',
+              '--from', 'nuclide',
+              '--json',
+              deps.filename
+          ], view)
+          if coverage_result:
+              regions = []
 
-        if coverage_result:
-            regions = []
+              for line in coverage_result['expressions']['uncovered_locs']:
+                  description = ''
 
-            for line in coverage_result['expressions']['uncovered_locs']:
-                description = ''
+                  start = line['start']
+                  end = line['end']
+                  row = int(start['line']) - 1
+                  col = int(start['column']) - 1
+                  endrow = int(end['line']) - 1
+                  endcol = int(end['column'])
+                  regions.append(rowcol_to_region(view, row, col, endcol, endrow))
 
-                start = line['start']
-                end = line['end']
-                row = int(start['line']) - 1
-                col = int(start['column']) - 1
-                endrow = int(end['line']) - 1
-                endcol = int(end['column'])
-                regions.append(rowcol_to_region(view, row, col, endcol, endrow))
+              view.add_regions(
+                  'flow_uncovered', regions, 'comment', 'dot',
+                  sublime.DRAW_STIPPLED_UNDERLINE + sublime.DRAW_NO_FILL + sublime.DRAW_NO_OUTLINE
+              )
 
-            view.add_regions(
-                'flow_uncovered', regions, 'comment', 'dot',
-                sublime.DRAW_STIPPLED_UNDERLINE + sublime.DRAW_NO_FILL + sublime.DRAW_NO_OUTLINE
-            )
-
-            uncovered_count = coverage_result['expressions']['uncovered_count']
-            covered_count_text = 'Flow coverage: {} line{} uncovered'.format(
-                uncovered_count, '' if uncovered_count is 1 else 's'
-            )
-            view.set_status('flow_coverage', covered_count_text)
+              uncovered_count = coverage_result['expressions']['uncovered_count']
+              covered_count_text = 'Flow coverage: {} line{} uncovered'.format(
+                  uncovered_count, '' if uncovered_count is 1 else 's'
+              )
+              view.set_status('flow_coverage', covered_count_text)
